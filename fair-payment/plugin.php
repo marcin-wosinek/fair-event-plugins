19fde849198259277d1cace6bc1ec0d1763537b4
Marcin Wosinek <marcin.wosinek@gmail.com>
2025-07-10 13:01:50 +0200

    update authorship info

--- fair-payment/plugin.php
+++ fair-payment/plugin.php
@@ -2,7 +2,7 @@
 /**
  * Fair Payment
  *
- * TODO: The description for the plugin scaffolded by this file.
+ * Simple payment plugint, with fair pricing model.
  *
  * PHP version 8.2
  *
@@ -10,13 +10,13 @@
  * @package  TODO
  * @author   Marcin Wosinek <marcin.wosinek@gmail.com>
  * @license  GPLv3 <https://www.gnu.org/licenses/gpl-3.0.en.html>
- * @link     https://github.com/marcin-wosinek/fair-payment
+ * @link     https://github.com/marcin-wosinek/fair-event-plugins
  * @since    TODO: Date
  *
  * @wordpress-plugin
  * Plugin Name: Fair Payment
- * Plugin URI:  https://github.com/marcin-wosinek/fair-payment
- * Description: The description for the plugin scaffolded by this file.
+ * Plugin URI:  https://github.com/marcin-wosinek/fair-event-plugins
+ * Description: Payment plugin, with fair pricing model.
  * Author:      Marcin Wosinek <marcin.wosinek@gmail.com>
  * Version:     1.0.0
  */
