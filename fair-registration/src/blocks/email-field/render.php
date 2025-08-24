9c5d1670f36c109ea1323e19701406cc0f07aee9
Marcin Wosinek <marcin.wosinek@gmail.com>
2025-08-24 10:56:11 +0200

    Add render functions

---
+++ fair-registration/src/blocks/email-field/render.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Server-side rendering for Email Field block
+ *
+ * @package FairRegistration
+ */
+
+$label = $attributes['label'] ?? 'Email Address';
+$placeholder = $attributes['placeholder'] ?? 'Enter your email';
+$required = $attributes['required'] ?? true;
+$field_id = $attributes['fieldId'] ?? 'email_' . uniqid();
+$form_id = $block->context['fair-registration/formId'] ?? '';
+
+$field_name = $form_id ? $form_id . '_' . $field_id : $field_id;
+?>
+
+<div class="wp-block-column fair-registration-field fair-registration-email-field">
+	<label for="<?php echo esc_attr($field_name); ?>" class="wp-block-label fair-registration-field-label">
+		<?php echo esc_html($label); ?>
+		<?php if ($required): ?>
+			<span class="required">*</span>
+		<?php endif; ?>
+	</label>
+	
+	<input
+		type="email"
+		id="<?php echo esc_attr($field_name); ?>"
+		name="<?php echo esc_attr($field_name); ?>"
+		class="wp-block-input fair-registration-field-input"
+		placeholder="<?php echo esc_attr($placeholder); ?>"
+		<?php if ($required): ?>required<?php endif; ?>
+	/>
+</div>
--- fair-registration/src/blocks/registration-form/render.php
+++ fair-registration/src/blocks/registration-form/render.php
@@ -9,7 +9,7 @@ $form_name = $attributes['name'] ?? '';
 $form_id = $attributes['id'] ?? '';
 $inner_blocks = $content ?? '';
 
-$form_classes = 'fair-registration-form';
+$form_classes = 'wp-block-group fair-registration-form';
 if (!empty($form_id)) {
 	$form_classes .= ' fair-registration-form-' . esc_attr($form_id);
 }
@@ -18,16 +18,20 @@ if (!empty($form_id)) {
 <div class="<?php echo esc_attr($form_classes); ?>">
 	<form 
 		id="<?php echo esc_attr($form_id ?: 'fair-registration-form-' . uniqid()); ?>" 
-		class="fair-registration-form-element"
+		class="wp-block-group fair-registration-form-element"
 		method="post"
 		action=""
 	>
-		<?php echo $inner_blocks; ?>
+		<div class="wp-block-columns fair-registration-form-fields">
+			<?php echo $inner_blocks; ?>
+		</div>
 		
-		<div class="fair-registration-form-actions">
-			<button type="submit" class="fair-registration-submit-btn">
-				<?php _e('Submit Registration', 'fair-registration'); ?>
-			</button>
+		<div class="wp-block-buttons fair-registration-form-actions">
+			<div class="wp-block-button">
+				<button type="submit" class="wp-block-button__link fair-registration-submit-btn">
+					<?php _e('Submit Registration', 'fair-registration'); ?>
+				</button>
+			</div>
 		</div>
 		
 		<?php wp_nonce_field('fair_registration_submit', 'fair_registration_nonce'); ?>
