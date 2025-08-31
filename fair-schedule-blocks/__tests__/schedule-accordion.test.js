5d2b42c20dadc3246b76586eb7df91e6fa0a8c6d
Marcin Wosinek <marcin.wosinek@gmail.com>
2025-08-31 21:46:41 +0200

    Generate the fair-schedule-blocks plugin

---
+++ fair-schedule-blocks/__tests__/schedule-accordion.test.js
@@ -0,0 +1,9 @@
+/**
+ * Test suite for Schedule Accordion block
+ */
+
+describe('Schedule Accordion Block', () => {
+	test('should have test placeholder', () => {
+		expect(true).toBeTruthy();
+	});
+});
---
+++ fair-schedule-blocks/src/Hooks/BlockHooks.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Block registration hooks for Fair Schedule Blocks
+ *
+ * @package FairScheduleBlocks
+ */
+
+namespace FairScheduleBlocks\Hooks;
+
+defined( 'WPINC' ) || die;
+
+/**
+ * Handles WordPress block registration and hooks
+ */
+class BlockHooks {
+
+	/**
+	 * Constructor - registers WordPress hooks
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'register_blocks' ) );
+	}
+
+	/**
+	 * Register all block types
+	 *
+	 * @return void
+	 */
+	public function register_blocks() {
+		// Blocks will be registered here when created
+		// Example:
+		// register_block_type( __DIR__ . '/../../build/blocks/schedule-item' );
+	}
+}
