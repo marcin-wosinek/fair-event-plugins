dc022e60d52e6b9254cef75b8f17df8c5728c7e9
Marcin Wosinek <marcin.wosinek@gmail.com>
2025-09-26 08:39:04 +0200

    Generate e2e test for membership

--- fair-membership/jest.config.js
+++ fair-membership/jest.config.js
@@ -7,36 +7,37 @@
 export default {
 	preset: '@wordpress/jest-preset-default',
 	testEnvironment: 'jsdom',
-	testMatch: [
-		'**/__tests__/**/*.js',
-		'**/?(*.)+(spec|test).js'
-	],
+	testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+	testPathIgnorePatterns: ['/node_modules/', '/vendor/', '/e2e/'],
 	collectCoverageFrom: [
 		'src/**/*.js',
 		'!src/**/index.js',
 		'!**/node_modules/**',
 		'!**/build/**',
-		'!**/vendor/**'
+		'!**/vendor/**',
+		'!**/e2e/**',
 	],
 	coverageDirectory: 'coverage',
-	coverageReporters: [
-		'text',
-		'lcov',
-		'html'
-	],
+	coverageReporters: ['text', 'lcov', 'html'],
 	setupFilesAfterEnv: [],
 	transform: {
-		'^.+\\.[jt]sx?$': ['babel-jest', {
-			presets: [
-				['@babel/preset-env', {
-					targets: {
-						node: 'current'
-					}
-				}]
-			]
-		}]
+		'^.+\\.[jt]sx?$': [
+			'babel-jest',
+			{
+				presets: [
+					[
+						'@babel/preset-env',
+						{
+							targets: {
+								node: 'current',
+							},
+						},
+					],
+				],
+			},
+		],
 	},
 	moduleNameMapper: {
-		'^@/(.*)$': '<rootDir>/src/$1'
-	}
+		'^@/(.*)$': '<rootDir>/src/$1',
+	},
 };
