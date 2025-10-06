3a53f24a7877e1ab32291ff7d27b20ce000d37ee
Marcin Wosinek <marcin.wosinek@gmail.com>
2025-10-06 07:56:02 +0200

    Improve support for setting removal

--- fair-events/src/Settings/Settings.php
+++ fair-events/src/Settings/Settings.php
@@ -21,6 +21,7 @@ class Settings {
 	public function init() {
 		add_action( 'init', array( $this, 'register_settings' ) );
 		add_action( 'update_option_fair_events_slug', array( $this, 'flush_rewrite_rules_on_slug_change' ), 10, 2 );
+		add_action( 'delete_option_fair_events_slug', array( $this, 'flush_rewrite_rules_on_slug_delete' ) );
 	}
 
 	/**
@@ -36,11 +37,29 @@ class Settings {
 			array(
 				'type'              => 'string',
 				'description'       => __( 'URL slug for events', 'fair-events' ),
-				'sanitize_callback' => 'sanitize_title',
+				'sanitize_callback' => array( $this, 'sanitize_slug' ),
 				'show_in_rest'      => true,
 				'default'           => 'fair-events',
 			)
 		);
+	}
+
+	/**
+	 * Sanitize slug value
+	 *
+	 * @param string $value Slug value to sanitize.
+	 * @return string Sanitized slug.
+	 */
+	public function sanitize_slug( $value ) {
+		$sanitized = sanitize_title( $value );
+
+		// If empty string, delete the option and return default
+		if ( empty( $sanitized ) ) {
+			delete_option( 'fair_events_slug' );
+			return 'fair-events';
+		}
+
+		return $sanitized;
 	}
 
 	/**
@@ -58,5 +77,17 @@ class Settings {
 			// Flush rewrite rules
 			flush_rewrite_rules();
 		}
+	}
+
+	/**
+	 * Flush rewrite rules when event slug is deleted (reset to default)
+	 *
+	 * @return void
+	 */
+	public function flush_rewrite_rules_on_slug_delete() {
+		// Re-register post type with default slug
+		\FairEvents\PostTypes\Event::register();
+		// Flush rewrite rules
+		flush_rewrite_rules();
 	}
 }
