1473cde17dc6665e1f3317daabfb27d5f1b4297e
Marcin Wosinek <marcin.wosinek@gmail.com>
2025-10-09 08:27:04 +0200

    Add translation files load

--- fair-events/src/Hooks/BlockHooks.php
+++ fair-events/src/Hooks/BlockHooks.php
@@ -19,6 +19,7 @@ class BlockHooks {
 	 */
 	public function __construct() {
 		add_action( 'init', array( $this, 'register_blocks' ) );
+		add_action( 'init', array( $this, 'set_script_translations' ) );
 	}
 
 	/**
@@ -29,5 +30,37 @@ class BlockHooks {
 	public function register_blocks() {
 		register_block_type( __DIR__ . '/../../build/blocks/events-list' );
 		register_block_type( __DIR__ . '/../../build/blocks/event-dates' );
+	}
+
+	/**
+	 * Set script translations for block editor scripts
+	 *
+	 * @return void
+	 */
+	public function set_script_translations() {
+		// WordPress auto-generates script handles based on block name and file
+		// Format: {namespace}-{block-name}-editor-script
+		$plugin_dir = plugin_dir_path( __DIR__ . '/../../fair-events.php' );
+
+		// Events list block editor script
+		wp_set_script_translations(
+			'fair-events-events-list-editor-script',
+			'fair-events',
+			$plugin_dir . 'build/languages'
+		);
+
+		// Event dates block editor script
+		wp_set_script_translations(
+			'fair-events-event-dates-editor-script',
+			'fair-events',
+			$plugin_dir . 'build/languages'
+		);
+
+		// Settings page script
+		wp_set_script_translations(
+			'fair-events-settings',
+			'fair-events',
+			$plugin_dir . 'build/languages'
+		);
 	}
 }
