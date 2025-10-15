359ff4f3cccbe0398db89444d0c0b8b9b486e37a
Marcin Wosinek <marcin.wosinek@gmail.com>
2025-10-15 09:29:01 +0200

    Show border around show-[unilt|after] blocks in editor

--- fair-schedule-blocks/src/blocks/show-until/editor.js
+++ fair-schedule-blocks/src/blocks/show-until/editor.js
@@ -2,6 +2,7 @@ import { registerBlockType } from '@wordpress/blocks';
 import Edit from './components/EditComponent.js';
 import Save from './components/SaveComponent.js';
 import metadata from './block.json';
+import './editor.css';
 
 registerBlockType(metadata.name, {
 	edit: Edit,
