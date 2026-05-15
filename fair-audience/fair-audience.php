ab848d03140bc57531b0d77c2b344640eb270f61
Marcin Wosinek <marcin.wosinek@gmail.com>
2026-05-15 11:06:09 +0200

    Tick the transacion on automatic matching

--- fair-audience/fair-audience.php
+++ fair-audience/fair-audience.php
@@ -3,7 +3,7 @@
  * Plugin Name: Fair Audience
  * Plugin URI: https://github.com/marcin-wosinek/fair-event-plugins
  * Description: Manage event participants with custom profiles and many-to-many event relationships
- * Version: 1.2.0-54-g58bb29b
+ * Version: 1.2.0-64-gb996bb9
  * Author: Marcin Wosinek
  * Author URI: https://github.com/marcin-wosinek
  * License: GPL-3.0-or-later
